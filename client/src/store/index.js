7021b9cc1d711f7ba33fc5fafdcf7df0979551e9
Vladimir Belyakov <belyakovvladimirs@gmail.com>
2025-01-20 21:30:16 +0300

    Refresh Git cache and apply .gitattributes

--- client/src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import goalsReducer from './goalsSlice';
-
-export const store = configureStore({
-  reducer: {
-    goals: goalsReducer,
-    // Add other reducers as needed
-  },
-}); 
